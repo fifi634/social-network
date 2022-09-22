c4948662b49ea5b8bea416a4fdd753c84a892e57
Philippe Cohen <cohen.philippe@hotmail.fr>
2022-09-22 18:13:23 +0200

    back feat: all user search functions creeated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,29 +13,32 @@ exports.getAllUsers = async (req, res) => {
 /* Find one user by id and return it sin password */
 exports.userInfo = (req, res) => {
     console.log('user info : ', req.params);
-    // Verification if 'id user requete' correspond with 'id user database'
-    if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send('ID unknown : ' + req.params.id);
+    // // Verification if 'id user requete' correspond with 'id user database'
+    // if (!ObjectID.isValid(req.params.id))
+    // return res.status(400).send('ID unknown : ' + req.params.id);
     
     UserModel.findById(req.params.id, (err, data) => {
-        if (!err) res.send(data);
-        else console.log('ID unknown : ' + err);
+        if (!err) res.status(200).json(data);
+        else res.status(404).json({ message:'Id unknown :', err });
     }).select('-password');
 }
 
 /* Find user by id and modify it */
 exports.updateUser = async (req, res) => {
-    // Verification if 'id user requete' correspond with 'id user database'
-    if (!ObjectID.isValid(req.params.id)) 
-    return res.status(400).send('ID unknown : ' + req.params.id);
+    // // Verification if 'id user requete' correspond with 'id user database'
+    // if (!ObjectID.isValid(req.params.id)) 
+    // return res.status(400).send('ID unknown : ' + req.params.id);
     
     try {
         UserModel.findOneAndUpdate( 
-            { _id: req.params.id }, {
-                $set: {
-                    bio: req.body.bio
-                }
-            }, { new: true, upsert: true, setDefaultsOnInsert: true },
+            { _id: req.params.id }, 
+            { $set: {
+                email: req.body.email,
+                password: req.body.password,
+                pseudo: req.body.pseudo,
+                avatar_slug: req.body.avatar_slug
+            } }, 
+            { new: true, upsert: true, setDefaultsOnInsert: true },
             (err, data) => {
                 if (!err) return res.send(data);
                 if (err) return res(500).send({ message: 'Update error: ', err });
@@ -46,14 +49,10 @@ exports.updateUser = async (req, res) => {
     };
 }
 
+/* Erase user */
 exports.deleteUser = async (req, res) => {
-    if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send('ID unknown : ' + req.params.id);
-
-    try {
-        await UserModel.remove({ _id: req.params.id }).exec();
-        res.status(200).json({ message: 'Successfully deleted.' });
-    } catch (err) {
-        return res.status(500).json({ message: 'Remove error: ', err })
-    };
+    UserModel.deleteOne({  _id: req.params.id })
+        .then(user => res.status(200).json({ message: 'Successfully deleted', user }))
+        .catch(error => res.status(400).json({ message: 'Delete failed', error }))
+    ;
 }
